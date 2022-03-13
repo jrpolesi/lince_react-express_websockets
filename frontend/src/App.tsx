613b85865586b91b0f1366c3aec899941829b940
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-03-13 17:05:57 -0300

    build: install framer-motion package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,38 @@
+import { StartGameModal } from "./components/StartGameModal";
+import { GameTable } from "./components/GameTable";
+import { observer } from "mobx-react-lite";
+import { useState } from "react";
+import { gameStore } from "./store/game";
+import { ImageCard } from "./components/ImageCard";
+import { Center, ChakraProvider } from "@chakra-ui/react";
+import customTheme from "./styles/custom-theme";
 
-function App() {
+const App = observer(() => {
+  const [isUserReady, setIsUserReady] = useState(false);
+
+  function toggleIsUserReady() {
+    setIsUserReady((prevIsReady) => !prevIsReady);
+  }
+
+  if (!isUserReady) {
+    return <StartGameModal toggleIsUserReady={toggleIsUserReady} />;
+  }
 
   return (
-    <div className="App">
-    </div>
+    <ChakraProvider theme={customTheme}>
+      {gameStore.result && <h1>{gameStore.result.winner.name}</h1>}
+      {gameStore.isGameReady ? (
+        <>
+          <Center >
+            <ImageCard image={gameStore.game.currentImage} />
+          </Center>
+          <GameTable />
+        </>
+      ) : (
+        <div>Aguardando os outros jogadores</div>
+      )}
+    </ChakraProvider>
   );
-}
+});
 
 export default App;
