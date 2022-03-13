45e11a1ad1f2e9554185ba2fc8d4e972af321110
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-03-13 01:29:30 -0300

    feat: Add StartGameModal component

---
+++ frontend/src/components/StartGameModal/index.tsx
@@ -0,0 +1,46 @@
+import { observer } from "mobx-react-lite";
+import { FormEvent, useState } from "react";
+import { gameStore } from "../../store/game";
+
+export const StartGameModal = observer(({toggleIsUserReady}:{toggleIsUserReady: () => void}) => {
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState<boolean | string>(false);
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const name = inputValue.trim()
+
+    if(!name.trim()){
+      return setError("Você deve escolher um nome")
+    }
+
+    const user = {
+      name,
+      image:
+        "https://flyclipart.com/thumb2/user-icon-png-pnglogocom-133466.png",
+    };
+
+    gameStore.startGame(user);
+    toggleIsUserReady()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="text"
+          name="name"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        {!!error && <label>{error}</label>}
+      </div>
+      <img
+        src="https://flyclipart.com/thumb2/user-icon-png-pnglogocom-133466.png"
+        alt="user avatar"
+      />
+      <button>Start</button>
+    </form>
+  );
+});
