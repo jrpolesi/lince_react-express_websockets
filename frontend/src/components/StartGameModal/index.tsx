b50fd0ac822f7d77ab71d0b3a70abda683c9bf1f
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-03-13 21:06:40 -0300

    refactor: use Box component as form

--- frontend/src/components/StartGameModal/index.tsx
+++ frontend/src/components/StartGameModal/index.tsx
@@ -1,46 +1,51 @@
+import { Box, Button, Image, Input } from "@chakra-ui/react";
 import { observer } from "mobx-react-lite";
-import { FormEvent, useState } from "react";
+import { FormEvent, FormEventHandler, useState } from "react";
 import { gameStore } from "../../store/game";
 
-export const StartGameModal = observer(({toggleIsUserReady}:{toggleIsUserReady: () => void}) => {
-  const [inputValue, setInputValue] = useState("");
-  const [error, setError] = useState<boolean | string>(false);
+export const StartGameModal = observer(
+  ({ toggleIsUserReady }: { toggleIsUserReady: () => void }) => {
+    const [inputValue, setInputValue] = useState("");
+    const [error, setError] = useState<boolean | string>(false);
 
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+    const handleSubmit: FormEventHandler = (
+      event: FormEvent<HTMLFormElement>
+    ) => {
+      event.preventDefault();
 
-    const name = inputValue.trim()
+      const name = inputValue.trim();
 
-    if(!name.trim()){
-      return setError("Você deve escolher um nome")
-    }
+      if (!name.trim()) {
+        return setError("Você deve escolher um nome");
+      }
 
-    const user = {
-      name,
-      image:
-        "https://flyclipart.com/thumb2/user-icon-png-pnglogocom-133466.png",
+      const user = {
+        name,
+        image:
+          "https://flyclipart.com/thumb2/user-icon-png-pnglogocom-133466.png",
+      };
+
+      gameStore.startGame(user);
+      toggleIsUserReady();
     };
 
-    gameStore.startGame(user);
-    toggleIsUserReady()
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <input
-          type="text"
-          name="name"
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
+    return (
+      <Box as="form" onSubmit={handleSubmit}>
+        <div>
+          <Input
+            type="text"
+            name="name"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+          {!!error && <label>{error}</label>}
+        </div>
+        <Image
+          src="https://flyclipart.com/thumb2/user-icon-png-pnglogocom-133466.png"
+          alt="user avatar"
         />
-        {!!error && <label>{error}</label>}
-      </div>
-      <img
-        src="https://flyclipart.com/thumb2/user-icon-png-pnglogocom-133466.png"
-        alt="user avatar"
-      />
-      <button>Start</button>
-    </form>
-  );
-});
+        <Button type="submit">Start</Button>
+      </Box>
+    );
+  }
+);
